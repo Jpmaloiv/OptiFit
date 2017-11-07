22168f101d1ff68f8e70fc8b5733ebbe62611595
Shant Wanes <swanes@bjsrestaurants.com>
2017-11-07 15:17:44 -0800

    BMI Display

--- assets/javascript/init.js
+++ assets/javascript/init.js
@@ -24,19 +24,39 @@ $(document).ready(function() {
    // $('#formBMI').submit(function(e) {
                      
 
-$("#testbmi").on("click", function(e) {
+$("#submit-button").on("click", function(e) {
         e.preventDefault();
         
-        userProfile.age = $("#age-input").val().trim();
-        userProfile.heightFt = $("#height-feet-input").val().trim();
-        userProfile.heightIn = $("#height-inches-input").val().trim();
+        userProfile.age = parseInt($("#age-input").val().trim());
+        userProfile.heightFt = parseInt($("#height-feet-input").val().trim());
+        userProfile.heightIn = parseInt($("#height-inches-input").val().trim());
         userProfile.height   = parseFloat( userProfile.heightFt * 12) + parseFloat(userProfile.heightIn);
-        userProfile.weight = $("#weight-input").val().trim();
+        userProfile.weight = parseInt($("#weight-input").val().trim());
         userProfile.BMI = userProfile.calculateBMI(userProfile.height, userProfile.weight);
-        console.log('Your BMI is '+ userProfile.BMI);
-
-        //$('divBMI').text('Your BMI is '+BMI);
+        $("#BMIBody").text('Your BMI is '+ userProfile.BMI);
+        //console.log('Your BMI is '+ userProfile.BMI);
 });
+
+$("#age-input").keypress(function(){
+       var response = validateAge($("#age-input").val());
+       console.log(response);
+});
+
+
+//validating if the age is a valid number
+function validateAge(age){
+ var ageNum = parseInt(age);
+if(ageNum == "")     
+    return false;
+
+//check if age is a number or less than or greater than 100
+if (isNaN(ageNum)||ageNum<1||ageNum>100)
+{ 
+    //alert("The age must be a number between 1 and 100");
+    return false;
+}
+}
+
 
         
 });
