cdb571740716e4d25ea6beef9a57f1d613086626
Adrian Nalwalker <analwalker@planetfg.local>
2017-11-07 19:04:13 -0800

    removed index.html file from commited files

--- assets/javascript/googlePlaces.js
+++ assets/javascript/googlePlaces.js
@@ -1,22 +1,25 @@
-var map;
+    var map;
     var infowindow;
     var request;
     var service;
     var markers = [];
+    var lat = -33.865936;
+    var lng = 151.216271;
 
+//initial map function:
     function myMapx() {
 
-        //specify the map properties
-
-        var center = new google.maps.LatLng(33.646131, -117.843041);
+        //specify the map location
+        var center = new google.maps.LatLng(lat, lng);
+        //create map and add it to the page
         var mapDiv = document.getElementById('googleMap');
-
+        //set the map placement and zoom
         map = new google.maps.Map(mapDiv, {
             center: center,
             zoom: 12
         });
 
-
+        //search for gyms within 5 miles or 8047 meters
         request = {
             location: center,
             radius: 8047,
@@ -26,13 +29,15 @@ var map;
 
         service = new google.maps.places.PlacesService(map);
 
-
         service.nearbySearch(request, callback);
 
+//listener to recenter map if right clicked
 google.maps.event.addListener(map, 'rightclick', function(event) {
   map.setCenter(event.latLng)
+  //clear existing markers
   clearResults(markers)
 
+  //find gyms again if map moved.
   var request = {
     location: event.latLng,
     radius: 8047,
@@ -42,6 +47,9 @@ google.maps.event.addListener(map, 'rightclick', function(event) {
 })
 
     }
+
+
+//hmmmm begin -------------------
 
     function callback(results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -58,6 +66,7 @@ google.maps.event.addListener(map, 'rightclick', function(event) {
             position: place.geometry.location
         });
 
+        //add info tag to marker if clicked
         google.maps.event.addListener(marker, 'click', function() {
           infowindow.setContent(place.name);
           infowindow.open(map, this);
@@ -71,6 +80,10 @@ google.maps.event.addListener(map, 'rightclick', function(event) {
       }
       markers = []
     
+
+    // hmmmmm end --------------
+
+
 
     // google.maps.event.addDomListener(window, 'load', myMapx());
 
@@ -105,31 +118,76 @@ google.maps.event.addListener(map, 'rightclick', function(event) {
      */
     // var infoWindow = new google.maps.InfoWindow;
 
-    // // Try HTML5 geolocation.
-    //         if (navigator.geolocation) {
-    //           navigator.geolocation.getCurrentPosition(function(position) {
-    //             var pos = {
-    //               lat: position.coords.latitude,
-    //               lng: position.coords.longitude
-    //             };
+    // Try HTML5 geolocation.
+            if (navigator.geolocation) {
+              navigator.geolocation.getCurrentPosition(function(position) {
+                var pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude
+                };
 
-    //             infoWindow.setPosition(pos);
-    //             infoWindow.setContent('Location found.');
-    //             infoWindow.open(map);
-    //             map.setCenter(pos);
-    //           }, function() {
-    //             handleLocationError(true, infoWindow, map.getCenter());
-    //           });
-    //         } else {
-    //           // Browser doesn't support Geolocation
-    //           handleLocationError(false, infoWindow, map.getCenter());
-    //         }
+                infowindow.setPosition(pos);
+                infowindow.setContent('Location found.');
+                infowindow.open(map);
+                map.setCenter(pos);
+              }, function() {
+                handleLocationError(true, infowindow, map.getCenter());
+              });
+            
+
+
+            } else {
+              // Browser doesn't support Geolocation
+              handleLocationError(false, infowindow, map.getCenter());
+            }
       
 
-    //       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    //         infoWindow.setPosition(pos);
-    //         infoWindow.setContent(browserHasGeolocation ?
-    //                               'Error: The Geolocation service failed.' :
-    //                               'Error: Your browser doesn\'t support geolocation.');
-    //         infoWindow.open(map);
-    //       }
+          function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+            infowindow.setPosition(pos);
+            infowindow.setContent(browserHasGeolocation ?
+                                  'Error: The Geolocation service failed.' :
+                                  'Error: Your browser doesn\'t support geolocation.');
+            infowindow.open(map);
+
+//search for gyms within 5 miles or 8047 meters
+        request = {
+            location: center,
+            radius: 8047,
+            types: ['gym']
+        }
+        infowindow = new google.maps.InfoWindow();
+
+        service = new google.maps.places.PlacesService(map);
+
+        service.nearbySearch(request, callback);
+
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                markers.push(createMarker(results[i]));
+            }
+        }
+    }
+
+    function createMarker(place) {
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
+
+        //add info tag to marker if clicked
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+        return marker;
+    }
+
+    function clearResults(markers) {
+      for (var m in markers) {
+        markers[m].setMap(null)}
+      }
+      markers = []
+
+          }
