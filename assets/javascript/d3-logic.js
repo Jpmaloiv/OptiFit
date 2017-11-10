fd2a311437981188ad04e6ee41b5a3b5d0bebb60
rdo <ryan@libertylearner.com>
2017-11-10 15:38:31 -0800

    setup for d3-logic.js

--- assets/javascript/d3-logic.js
+++ assets/javascript/d3-logic.js
@@ -1,20 +1,4 @@
-// google.charts.load("current", {packages:["corechart"]});
-// google.charts.setOnLoadCallback(drawChart);
-// function drawChart() {
-//   var data = google.visualization.arrayToDataTable([
-//     ['Task', 'Hours per Day'],
-//     ['Work',     11],
-//     ['Eat',      2],
-//     ['Commute',  2],
-//     ['Watch TV', 2],
-//     ['Sleep',    7]
-//   ]);
+function drawBarGraph(){
+    console.log('DRAWING BRA GRAPH USING D3')
 
-//   var options = {
-//     title: 'My Daily Activities',
-//     is3D: true,
-//   };
-
-//   var chart = new google.visualization.PieChart(document.getElementById('caloriesBody'));
-//   chart.draw(data, options);
-// }
+};
